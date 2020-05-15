db9846d839b3a7bcc20ed0e0c448d9571f47ce6e
Gama Aristondo <gamaliel@cs.ucsb.edu>
2020-05-15 16:41:16 -0700

    finished

--- usestack.cpp
+++ usestack.cpp
@@ -1,5 +1,5 @@
 // usestack.cpp - for CS 24 lab practice using stacks
-// NAME(S), DATE
+// Gama Aristondo 5/15/20
 
 #include "intstack.h"
 #include <iostream>
@@ -18,6 +18,45 @@ int main() {
         s.pop();
     }
     cout << s.empty() << endl;
+
+    // Evaluating (((8*8)/2) - 4)
+    // "8 8 * 2 / 4 -"
+    // empty stack
+    Stack numbers;
+
+    // Push first two numbers
+    numbers.push(8);
+    numbers.push(8);
+
+    // Next is * operator
+    int right = numbers.top();
+    numbers.pop();
+    int left = numbers.top();
+    numbers.pop();
+    numbers.push(right * left);
+
+    // Push next number
+    numbers.push(2);
+
+    // Next operator is /
+    right = numbers.top();
+    numbers.pop();
+    left = numbers.top();
+    numbers.pop();
+    numbers.push(left / right);
+
+    // Push next number
+    numbers.push(4);
+
+    // Next operator is -
+    right = numbers.top();
+    numbers.pop();
+    left = numbers.top();
+    numbers.pop();
+    numbers.push(left - right);
+
+    // Result
+    cout << numbers.top() << endl;
 
     return 0;
 }
